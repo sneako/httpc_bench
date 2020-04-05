b2ecbe05e1cd4a9267cc01696df8bcfac0456726
Nico Piderman <nico.piderman@gmail.com>
2020-04-05 22:57:03 +0200

    fix path

--- lib/httpc_bench/config.ex
+++ lib/httpc_bench/config.ex
@@ -58,7 +58,7 @@ defmodule HttpcBench.Config do
   end
 
   def url do
-    "http://#{@host}:#{@port}/#{@path}"
+    "http://#{@host}:#{@port}#{@path}"
   end
 
   @buoy_url {:buoy_url, "#{@host}:#{@port}", @host, @path, @port, :http}
