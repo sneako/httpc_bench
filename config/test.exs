b7231a8639593d63ee938fd57af6dd66392dd3ad
Nico Piderman <nico.piderman@gmail.com>
2020-04-07 21:20:39 +0200

    implement buoy.post

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ config :httpc_bench,
   iterations: 10_000,
   concurrencies: [256, 512],
   pool_sizes: [20, 50, 100],
-  pool_counts: [4, 8, 16]
+  pool_counts: [1, 4, 8, 16]
 
 # concurrencies: [256, 512, 1024],
 # pool_sizes: [20, 50, 100],
