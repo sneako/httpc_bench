2e8cd1bae263224560fcbbad8ec27f0673f7c079
Nico Piderman <nico.piderman@gmail.com>
2020-04-17 13:41:12 +0200

    ...

--- config/test.exs
+++ config/test.exs
@@ -5,12 +5,18 @@ use Mix.Config
 #   concurrencies: [128, 256, 512],
 #   pool_sizes: [20, 50, 100],
 #   pool_counts: [4, 8, 16]
-
+#
 config :httpc_bench,
   iterations: 10_000,
-  concurrencies: [128, 256, 512, 1024],
-  pool_sizes: [50, 100, 150],
-  pool_counts: [4, 8, 16]
+  concurrencies: [256, 512],
+  pool_sizes: [20, 50, 100],
+  pool_counts: [1, 4, 8, 16]
+
+# config :httpc_bench,
+#   iterations: 10_000,
+#   concurrencies: [128, 256, 512, 1024],
+#   pool_sizes: [50, 100, 150],
+#   pool_counts: [4, 8, 16]
 
 # concurrencies: [256, 512, 1024],
 # pool_sizes: [20, 50, 100],
