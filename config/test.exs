f41bf5930d914be5883eb504e54c3619feea4087
Nico Piderman <nico.piderman@gmail.com>
2020-07-12 15:05:06 -0700

    config

--- config/test.exs
+++ config/test.exs
@@ -4,4 +4,6 @@ config :httpc_bench,
   iterations: 10_000,
   concurrencies: [512, 256],
   pool_sizes: [128, 96, 64, 32],
-  pool_counts: [32, 24, 16, 8]
+  pool_counts: [32, 24, 16, 8],
+  host: "localhost",
+  port: 8080
